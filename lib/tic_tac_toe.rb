7678b7633d968674cf10a2b78c96d8fc1de2166a
James Harris <mharris9608@yahoo.com>
2020-12-08 00:37:51 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,76 @@
+def valid_move?(board, index)
+  if position_taken?(board, index)
+  else
+    index.between?(0, 8)
+  end
+end
+
+def position_taken?(board, index)
+board[index] == " " || board[index] == "" || board[index] == nil
+board[index] == "X" || board[index] == "O"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  converted_input = (user_input.to_i) - 1
+end
+
+def move(board, input, value = 'X')
+  def update_array_at_with(board, input, value = 'X')
+    board[input] = value
+  end
+  update_array_at_with(board, input, value)
+end
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win|
+    board[win[0]] == board[win[1]] &&
+    board[win[1]] == board[win[2]] &&
+    position_taken?(board, win[0])
+  end
+end
+
+def full?(board)
+  board.all?{|full| full == "X" || full == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  else won?(board)
+    return false
+  end
+end
+
+def over?(board)
+  if draw?(board) || won?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
