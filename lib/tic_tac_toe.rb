c2cac37307066eea45e44dd58d8bc555052cc38f
James Harris <mharris9608@yahoo.com>
2020-12-08 01:53:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,14 +72,14 @@ def winner(board)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    move(board, input, current_player(board))
-    turn(board)
-  end
-    display_board(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+      move(board, index, current_player(board))
+      turn(board)
+   end
+   display_board(board)
 end
 
 def turn_count(board)
