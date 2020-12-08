5b7392000abe64d7ebeda6268352941043b00609
James Harris <mharris9608@yahoo.com>
2020-12-08 01:08:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -107,4 +107,10 @@ def play(board)
   until over?(board)
     turn(board)
   end
+  if won?(board)
+    winner(board) == "X" || winner(board) == "O"
+    puts "Congratulations #{winner(board)!}"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
 end
