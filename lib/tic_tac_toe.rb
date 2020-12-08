fc7a6452e750725987fdf10281f40f5f25f4beec
James Harris <mharris9608@yahoo.com>
2020-12-08 00:44:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,3 +86,21 @@ def turn(board)
   end
     display_board(board)
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |turns|
+    if turns == "X" || turns == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
